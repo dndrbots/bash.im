1bbb240acc6e2f1416bdf5a2b8aabac621789005
nitrojs <n1trojscoding@mail.ru>
2018-12-02 12:58:55 +0300

    idk how to describe this lul

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch');
 let iconv = require('iconv-lite');
 
+let idRegexp = /<a href="\/quote\/.*" class="id">#(.*)<\/a>/i;
+let regexp = /<div class="text">([^]+?)<\/div>/i;
+
 async function bash() {
   let result = await (await fetch('https://bash.im/random')).buffer();
   result = iconv.decode(result, 'win1251');
 
-  let idRegexp = /<a href="\/quote\/.*" class="id">#(.*)<\/a>/i;
   let id = +result.match(idRegexp)[1];
-  
-  let regexp = /<div class="text">([^]+?)<\/div>/i;
   let text = result.match(regexp)[1];
   text = text.replace(/<br(?:\s*\/)?>/ig, '\n')
     .replace(/&quot;/g, '"')
